87e9e049b59005a753320cc06bbcdf4bb59a3639
harbouli <mharbouli@icloud.com>
2023-04-24 16:31:13 +0100

    Docker Modifie

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  await app.listen(5000, () => Logger.log('Server Runing On PORT 5000'));
 }
 bootstrap();
